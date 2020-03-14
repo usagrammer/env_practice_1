90e557ca29bb68b8f716aabdfdbf073b234f2b81
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-03-14 18:47:40 +0900

    initial commit

---
+++ script.sh
@@ -0,0 +1,19 @@
+if [ `echo $SHELL | grep 'zsh'` ] ; then
+  ENV_FILE="zshrc"
+elif [ `echo $SHELL | grep 'fish'` ] ; then
+  printf "
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+fishは対応してないよ！！
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"
+  return 2>&- || exit
+else
+  ENV_FILE="bash_profile"
+fi
+
+sed -i .bak '/rbenv init/d' ~/.$ENV_FILE
+
+printf "
+仕込み完了です。
+一旦ターミナルを再起動してください
+"
